25ae113e3cc2c8f349070462460a5114260aac9b
Aman Yadav <192107aman@gmail.com>
2026-06-19 21:59:58 +0530

    Reverse String

--- Arrays/Reverse String/reverse_string.py
+++ Arrays/Reverse String/reverse_string.py
@@ -1,10 +1,9 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        tmp = []
-        for i in range(len(s)-1, -1, -1):
-            tmp.append(s[i])
-        for i in range(len(s)-1,-1,-1):
-            s[i] = tmp[i]
+        def reverseString(self, s: List[str]) -> None:
+            left = 0
+            right = len(s) - 1
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+            return s
